e4204fd2e9e6be2c3f5699f74c617377789359e8
Hunters3rd <hunters3rd@yahoo.fr>
2016-02-10 11:05:22 +0100

    Maj le 10/02/16 seconde modification.

--- Server/main.c
+++ Server/main.c
@@ -5,28 +5,28 @@
 int main() {
 	char *message = NULL;
 
-	/*MARQ_ESSAI
-	message = "CONNECT Alexis 0001";*/
+	/*MARQ_ESSAI*/
+	message = "CONNECT Alexis 0001";
 
 	Initialisation();
 
 	while(1) {
 		int fini = 0;
-	
-		/*MARQ_ESSAI */AttenteClient();
-	
+
+		/*MARQ_ESSAIAttenteClient();*/
+
 		while(!fini) {
-			/*MARQ_ESSAI */message = Reception();
+			/*MARQ_ESSAI message = Reception();*/
 
 			if(message != NULL) {
 				printf("J'ai recu: %s\n", message);
 
 				/* Le Serveur traite le message reçu */
-				traitement(message); 
+				traitement(message);
 
 				/* Le Serveur détruit le message */
-				/*MARQ_ESSAI */free(message);
-		
+				/*MARQ_ESSAI free(message);*/
+
 				if(Emission("Test de message serveur.\n")!=1) {
 					printf("Erreur d'emission\n");
 				}
