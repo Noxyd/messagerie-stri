8ec430b9396d4869a7147a9a19103cad74deeb8a
Samuel Garcia <samuel.garcia01@outlook.com>
2016-02-11 08:40:28 +0100

    v1 ecrire.c

--- Client/ecrire.c
+++ Client/ecrire.c
@@ -2,10 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define t_buffer 1024
+
 /* Writing a e-mail. */
 int main(){
   /* Variables */
+  char ligne[t_buffer] = {0};
+  FILE* corps = NULL;
 
   /* Begin */
+  corps = fopen("tmp_mail_w.txt","w");
+  if(corps == NULL)
+    return 1;
 
+  puts("[Nouveau message]");
+
+  printf("TO : ");
+  fgets(ligne, 120, stdin);
+
+  fprintf(corps, "TO: %s\n", ligne);
+
+  fclose(corps);
+
+  return 0;
 }
